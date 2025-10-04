301b66c1bcfbd6e35edde10960935d67a2efea5b
Milan <76396495+themilan1337@users.noreply.github.com>
2025-10-04 18:45:30 +0500

    v6

--- frontend/app/pages/dashboard.vue
+++ frontend/app/pages/dashboard.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
       <Header />
+      <div class="flex">
+        <div class="h-[calc(100vh-1.8rem)] border-r border-zinc-300 border-dashed w-1/2"></div>
+        <div class="h-[calc(100vh-1.8rem)] w-1/2"></div>
+      </div>
     </div>
 </template>
 
